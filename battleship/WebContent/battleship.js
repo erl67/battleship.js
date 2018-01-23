077ff2a3bac1dfaa4c51260d076e08dc1c6e195f
erl67 <erl67@pitt.edu>
2018-01-22 19:39:19 -0500

    build oceans

--- battleship/WebContent/battleship.js
+++ battleship/WebContent/battleship.js
@@ -13,17 +13,36 @@ function intro(obj) {
 
 function gameBoard(output) {
 	board = new Array(new Array());
+	board2 = new Array();
 	
 	var output = document.getElementById(output);
+	
+	table = document.createElement("table");
 
 	for (i = 1; i <= boardSize; i++) {
+		tableRow = document.createElement("tr");
+		
 		for (j = 0; j < boardSize; j++) {
 //		    board[i][j] = board.push(i,j);
 //		    output.append(board[i][j]);
-		    output.append(i + cols[j]);
-		    output.append("  ");
+			
+//		    output.append(i + cols[j]);
+//		    output.append("  ");
+//			board2.push(i.toString + cols[j].toString);
+			
+		    tableData = document.createElement("td");
+		    tableData.innerHTML = i.toString() + cols[j].toString();
+		    tableRow.append(tableData);
 		}
-		output.append(document.createElement("br"));
+		table.append(tableRow);
+		
+//		output.append(document.createElement("br"));
 	}
+	output.append(table);
+	
+	for (space in board2) {
+		document.write(space);
+	}
+	
 	
 }

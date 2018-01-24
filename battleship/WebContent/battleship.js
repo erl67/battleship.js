2504ecbdfefd5669eb2c5579ee541107adc6e4e5
erl67 <erl67@pitt.edu>
2018-01-24 18:27:39 -0500

    add hit

--- battleship/WebContent/battleship.js
+++ battleship/WebContent/battleship.js
@@ -26,9 +26,7 @@ function gameBoard(output) {
 		for (j = 0; j < boardSize; j++) {
 //		    board[i][j] = board.push(i,j);
 //		    output.append(board[i][j]);
-			
 //		    output.append(i + cols[j]);
-//		    output.append("  ");
 //			board2.push(i.toString + cols[j].toString);
 			
 		    tableData = document.createElement("td");
@@ -44,28 +42,20 @@ function gameBoard(output) {
 				
 				alert("You clicked: " + e.currentTarget.innerHTML);
 				
+				var hit = Math.random() >= 0.5;
+				
 				if (hit == false) {
 					document.getElementById(e.currentTarget.innerHTML).setAttribute("style", "background-color:#c0c0c0");
 				} else {
 					document.getElementById(e.currentTarget.innerHTML).setAttribute("style", "background-color:#cc0000");
 				}
 				
-				
-				//document.getElementById(e.currentTarget).removeEventListener("click", null);
-				//e.currentTarget.removeEventListener("click", null);
-				//e.currentTarget.addEventListener("click", null, true);
-				
-				//document.getElementById(e.currentTarget.innerHTML).removeEventListener("click", function(e) {return false;});
-				//document.getElementById(e.currentTarget.innerHTML).addEventListener("dblclick", function(e) {return false;}, true);
-
-				//e.setAttribute("style", "background-color:#c0c0c0");
 			})
 
 		    tableRow.append(tableData);
 		}
 		table.append(tableRow);
 		
-//		output.append(document.createElement("br"));
 	}
 	output.append(table);
 	
